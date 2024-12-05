c2df59f8c5d7a02ec72ffd8ee571d52dddf9317d
Byung-moon <bm4655@naver.comn>
2024-12-05 22:03:07 +0900

    1. 메인이미지 업데이트

--- automotive/src/main/java/com/example/myapplication/MainActivity.kt
+++ automotive/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.widget.LinearLayout
+import android.widget.ImageButton
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val naviButton = findViewById<LinearLayout>(R.id.naviButton)
-        val historyButton = findViewById<LinearLayout>(R.id.historyButton)
-        val gameButton = findViewById<LinearLayout>(R.id.gameButton)
+        val naviButton = findViewById<ImageButton>(R.id.naviButton)
+        val historyButton = findViewById<ImageButton>(R.id.historyButton)
+        val gameButton = findViewById<ImageButton>(R.id.gameButton)
 
         naviButton.setOnClickListener {
             val intent = Intent(this, NaviActivity::class.java)
